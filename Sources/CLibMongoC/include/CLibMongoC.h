24b7bca52e39c54cff5d293c43c5295a29804a31
Nikolay Dzhulay <nikolay.july@gmail.com>
2025-10-19 19:40:33 +0100

    Try to silence warning about missing imports

--- Sources/CLibMongoC/include/CLibMongoC.h
+++ Sources/CLibMongoC/include/CLibMongoC.h
@@ -4,4 +4,19 @@
 #include "CLibMongoC_bson.h"
 #include "CLibMongoC_mongoc.h"
 
+#import "CLibMongoC_common-config.h"
+#import "CLibMongoC_common-prelude.h"
+#import "CLibMongoC_common-macros-private.h"
+#import "CLibMongoC_common-b64-private.h"
+#import "CLibMongoC_common-md5-private.h"
+#import "CLibMongoC_common-thread-private.h"
+
+#import "CLibMongoC_mongoc-stream-tls.h"
+#import "CLibMongoC_mongoc-stream-tls-libressl.h"
+#import "CLibMongoC_mongoc-stream-tls-openssl.h"
+#import "CLibMongoC_mongoc-stream-tls-secure-channel.h"
+#import "CLibMongoC_mongoc-stream-tls-secure-transport.h"
+#import "CLibMongoC_mongoc-rand.h"
+#import "CLibMongoC_mongoc-ssl.h"
+
 #endif // C_LIBMONGOC_H
